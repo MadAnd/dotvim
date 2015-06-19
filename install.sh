1816cdae6b3effcff4de6913d7254e333dc69aa5
MadAnd <dev@madand.net>
2015-06-19 19:21:47 +0300

    install.sh: refactoring, improved comments.

--- install.sh
+++ install.sh
@@ -2,9 +2,17 @@
 set -e
 
 GIT_REPO="https://github.com/MadAnd/dotvim.git"
-INSTALL_PATH=~/.vim
+INSTALL_DIR=~/.vim
 VIMRC=~/.vimrc
 
+# USAGE
+#   find_executable executable-name
+# DESCRIPTION
+#   Find an executable, given as a first argument, in PATH
+#   and assign the found path to a global variable.
+# EXAMPLES
+#  `find_executable git` - will set the variable GIT_CMD to e.g. '/usr/bin/git'
+#  `find_executable bash` - will set the variable BASH_CMD, etc.
 find_executable()
 {
     if (( $# != 1 )); then
@@ -29,26 +37,24 @@ find_executable vim
 # Generate GIT_CMD
 find_executable git
 
-# backup previous vim config, or remove symlinks
+# Backup previous vim config, or delete if they are symlinks
 if [[ -h "$VIMRC" ]]; then
     rm "$VIMRC"
 elif [[ -f "$VIMRC" ]]; then
     mv -f "$VIMRC" "$VIMRC".bak
 fi
 
-if [[ -h "$INSTALL_PATH" ]]; then
-    rm "$INSTALL_PATH"
-elif [[ -d "$INSTALL_PATH" ]]; then
-    mv -f "$INSTALL_PATH" "$INSTALL_PATH".bak
+if [[ -h "$INSTALL_DIR" ]]; then
+    rm "$INSTALL_DIR"
+elif [[ -d "$INSTALL_DIR" ]]; then
+    mv -f "$INSTALL_DIR" "$INSTALL_DIR".bak
 fi
 
+# Install the config
+$GIT_CMD clone --recursive "$GIT_REPO" "$INSTALL_DIR"
+ln -sf "$INSTALL_DIR/vimrc" "$VIMRC"
 
-# install new config
-$GIT_CMD clone --recursive "$GIT_REPO" "$INSTALL_PATH"
-ln -sf "$INSTALL_PATH/vimrc" "$VIMRC"
-
-
-# let Vundle install all other plugins
+# Let Vundle install all the plugins
 $VIM_CMD +PluginInstall +qall
 
 echo 'Installation done!'
